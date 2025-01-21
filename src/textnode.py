8d8ac7a13f26a66b2964e473cb219ae82b5a9924
Drosos <drosinakis.drosos1@gmail.com>
2025-01-21 22:59:50 +0200

    start the static site generator project

---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    pass
+
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == other.text \
+                and self.text_type == other.text_type \
+                and self.url == other.url:
+            return True
+        return False
+
+    # TODO: change it later to take the value self.text_type.value
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
