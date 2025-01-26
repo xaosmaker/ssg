b0edbdebe00fbce7c8410a490c8ce4661dbdcfac
Drosos <drosinakis.drosos1@gmail.com>
2025-01-26 12:37:56 +0200

    finish project

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,7 +29,7 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             raise ValueError("value required")
         if not self.tag:
             return self.value
